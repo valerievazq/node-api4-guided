3b1bfd7b765fe85d6e21b9721aa322958af08f64
Valerie Vazquez <vnvazquez68@gmail.com>
2020-08-13 20:41:26 -0600

    added heroku port

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({
     message: `Welcome ${process.env.COHORT}`,
-    quote: process.env.QUOTE_OF_THE_DAY || "be good, or be good at it",
+    quote: process.env.QUOTE_OF_THE_DAY || "Be good, or be good at it",
   });
 });
 
